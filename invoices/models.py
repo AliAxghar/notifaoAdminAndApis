c162d11bc77e577931c2e0aa5a951204d69a414a
AliAxghar <d.ali679asghar@gmail.com>
2020-12-21 17:50:17 +0500

    stripe and invoices-page-12-21-20

--- invoices/models.py
+++ invoices/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
 class Invoices(models.Model):
-    billing_id = models.CharField(max_length=255,null=True)
-    name = models.CharField(max_length=100,null=True)
-    price = models.IntegerField(null= False)
-    interval = models.CharField(max_length= 50,null=False)
-    notifications = models.IntegerField(null=False)
-    apps = models.IntegerField(null=False)
-    description = models.TextField (null=True)
-    email = models.CharField(max_length=100,null=True)
-    status  = models.BooleanField(default=True)
-    payment_method = models.CharField(max_length=100,null=True)
+    billing_id = models.CharField(max_length=255,null=True,blank=True)
+    name = models.CharField(max_length=255,null=True,blank=True)
+    price = models.IntegerField(null= True,blank=True)
+    interval = models.CharField(max_length= 255,null=True)
+    notifications = models.IntegerField(null= True,blank=True)
+    apps = models.IntegerField(null= True,blank=True)
+    description = models.CharField (max_length=555,null= True,blank=True)
+    email = models.CharField(max_length=255,null= True,blank=True)
+    status  = models.CharField(max_length=255,null= True,blank=True)
+    payment_method = models.CharField(max_length=255,null= True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(null = True)
-    charge_id = models.CharField(max_length=255,null=True)
-    customer_id = models.CharField(max_length=255,null=True)
-    subscription_id = models.CharField(max_length=255,null=True)
-    subscription_status = models.CharField(max_length=100,null=True)
-
+    updated_at = models.DateTimeField(auto_now_add=True)
+    charge_id = models.CharField(max_length=255,null= True,blank=True)
+    customer_id = models.CharField(max_length=255,null= True,blank=True)
+    subscription_id = models.CharField(max_length=255,null= True,blank=True)
+    subscription_created_at = models.CharField(max_length=255,null= True,blank=True)
+    charge_receipt_url = models.CharField(max_length=555,null= True,blank=True)
     def __str__(self):
         return self.email
 
