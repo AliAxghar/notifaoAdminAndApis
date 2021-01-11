1e201f943b579bc3a7ee38f090e4685325198b1d
AliAxghar <d.ali679asghar@gmail.com>
2021-01-11 22:33:30 +0500

    registration ajax call is done 1-11-21

--- authentication/views.py
+++ authentication/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth.models import User
 from django.forms.utils import ErrorList
 from django.http import HttpResponse
 from .forms import LoginForm, SignUpForm
+from django.http import JsonResponse
 from customers.models import Customer
 from core.settings import api_base_url
 import requests
@@ -37,18 +38,65 @@ def login_view(request):
 
     return render(request, "accounts/login.html", {"form": form, "msg" : msg})
 
+# def register_user(request):
+
+#     msg     = None
+#     msg1     = None
+#     success = False
+
+#     if request.method == "POST":
+#         phone = request.POST.get('phone_number')
+#         name = request.POST.get('name')
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+#         password2 = request.POST.get('password2')
+#         get_customer_email = Customer.objects.filter(email=email)
+#         get_customer_phone = Customer.objects.filter(phone=phone)
+#         if get_customer_email:
+#             msg = "User is already exist with this email"
+#         else:
+#             if get_customer_phone:
+#                 msg = "This phone number is already exist"
+#             else:
+#                 if password==password2:
+#                     if len(password) < 8:
+#                         msg = "Password is too short, must contain at least 8 characters."
+#                     else:
+#                         data = {"name":name, "phone":phone, "email":email, "password1":password, "password2":password, "business_name":"None"}
+#                         createCustomer = requests.post('{}/customer/register/'.format(api_base_url), data=data)
+#                         res = createCustomer.json()
+#                         pass_validate = res
+#                         if res:
+#                             if len(res) > 1:
+#                                 return redirect('/login/')
+#                             else:
+#                                 msg = "This password is too common."
+#                         # customer = Customer.objects.create(name=name, phone=phone, email=email, password=password)                            
+#                 else:
+#                     msg = "Passwords do not match"
+
+#     return render(request, "accounts/register.html", {"msg" : msg, "success" : success })
+
+
 def register_user(request):
 
     msg     = None
     msg1     = None
     success = False
-
-    if request.method == "POST":
-        phone = request.POST.get('phone_number')
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        password2 = request.POST.get('password2')
+    # if request.method == "POST":
+    # phone = request.POST.get('phone_number')
+    # name = request.POST.get('name')
+    # email = request.POST.get('email')
+    # password = request.POST.get('password')
+    # password2 = request.POST.get('password2')
+    userdatalist = request.GET.getlist('list[]')
+    if userdatalist:
+        name = userdatalist[0]
+        email = userdatalist[1]
+        phone = userdatalist[2]
+        password = userdatalist[3]
+        password2 = userdatalist[4]
+        # print(name,email,phone,password,password2)
         get_customer_email = Customer.objects.filter(email=email)
         get_customer_phone = Customer.objects.filter(phone=phone)
         if get_customer_email:
@@ -73,19 +121,7 @@ def register_user(request):
                         # customer = Customer.objects.create(name=name, phone=phone, email=email, password=password)                            
                 else:
                     msg = "Passwords do not match"
-        # form = SignUpForm(request.POST)
-        # if form.is_valid():
-        #     form.save()
-        #     email = form.cleaned_data.get("email")
-        #     raw_password = form.cleaned_data.get("password1")
-        #     user = authenticate(email=email, password=raw_password)
-
-        #     msg     = 'User is created successfully'
-        #     success = True
-            
-        #     # return redirect("/login/")
-
-        # else:
-        #     msg1 = 'Form is not valid'
+        data = { 'msg': msg, 'data': userdatalist }
+        return JsonResponse(data)
 
     return render(request, "accounts/register.html", {"msg" : msg, "success" : success })
